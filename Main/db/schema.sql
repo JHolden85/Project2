67ba1bd8cef36e7aaac23d7f5dc914a5066e3b76
Gargantua <martinzb949@gmail.com>
2020-12-19 09:36:13 -0800

    rewired the the currect platform to fit our game, changed the home page to match our ideas

--- Main/db/schema.sql
+++ Main/db/schema.sql
@@ -1,2 +1,2 @@
-DROP DATABASE IF EXISTS crowdfund_db;
-CREATE DATABASE crowdfund_db;
+DROP DATABASE IF EXISTS trivia_db;
+CREATE DATABASE trivia_db;
--- Main/server.js
+++ Main/server.js
@@ -15,13 +15,13 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
 };
 
 app.use(session(sess));
@@ -37,5 +37,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+    app.listen(PORT, () => console.log('Now listening'));
 });
