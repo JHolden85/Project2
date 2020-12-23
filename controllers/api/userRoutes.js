5ab5e781305fe3d7fe6bc1c0278ff4d6c208d85e
perezcarlos93 <perez.carlos_a@yahoo.com>
2020-12-22 20:48:46 -0800

    added basic html templates with handlebars and added routes to each page in homeRoutes.js

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-router.post('/', async(req, res) => {
+router.post('/', async (req, res) => {
     try {
         const userData = await User.create(req.body);
 
@@ -16,7 +16,7 @@ router.post('/', async(req, res) => {
     }
 });
 
-router.post('/login', async(req, res) => {
+router.post('/login', async (req, res) => {
     try {
         const userData = await User.findOne({
             where: { email: req.body.email },
