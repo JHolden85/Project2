3edae3d9490cab5af74c3a197094abdb78565109
Joshua Pribek <joshpribek@icloud.com>
2021-01-05 18:39:29 -0800

    routers

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -6,11 +6,11 @@ const musicRoutes = require('./musicRoutes');
 const sportsRoutes = require('./sportsRoutes');
 const televisionRoutes = require('./televisionRoutes');
 
-router.use('/celeberties', celebertiesRoutes);
-router.use('/history', historyRoutes);
-router.use('/movies', moviesRoutes);
-router.use('/music', musicRoutes);
-router.use('/sports', sportsRoutes);
-router.use('/television', televisionRoutes);
+// router.use('/celeberties', celebertiesRoutes);
+// router.use('/history', historyRoutes);
+// router.use('/movies', moviesRoutes);
+// router.use('/music', musicRoutes);
+// router.use('/sports', sportsRoutes);
+// router.use('/television', televisionRoutes);
 
 module.exports = router;
