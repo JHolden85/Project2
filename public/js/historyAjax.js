d56f906e9bdf7143500b350f4b301d6a89e28a77
perezcarlos93 <perez.carlos_a@yahoo.com>
2021-01-07 21:23:32 -0800

    Score counter fully functional. When quiz is over, redirects to '/end' route which is yet to be created

--- public/js/historyAjax.js
+++ public/js/historyAjax.js
@@ -7,44 +7,58 @@ const answer4 = document.getElementById('answer4');
 // AJAX Call URL
 var url = 'https://opentdb.com/api.php?amount=10&category=23';
 
-// Placeholder for selected answer
-var selected = null;
-
 // Defining Score element and counter
 var score = 0;
 const scoreElem = document.getElementById('score');
-scoreElem.innerHTML = 'Your score: ' + score;
 
 // Var index acts as the index of the array received from the AJAX request
 var index = 0;
 
+// Function to populate question and answers into HTML div
 function populateQuiz(res, index) {
     if (res.results[index].type === 'multiple') {
+        // Compiles all answers both correct/incorrect into array
         var questionArray = [];
         questionArray.push(res.results[index].correct_answer);
         questionArray.push(res.results[index].incorrect_answers[0]);
         questionArray.push(res.results[index].incorrect_answers[1]);
         questionArray.push(res.results[index].incorrect_answers[2]);
 
-        // console.log(questionArray);
-
+        // Populates AJAX question into question element
         question.innerHTML = res.results[index].question;
+
+        //Populates answers from questionArray into answer divs, then removes the last used question to avoid repeats
         answer1.innerHTML =
             questionArray[Math.floor(Math.random() * questionArray.length)];
         questionArray = questionArray.filter((e) => e !== answer1.innerText);
-        // console.log(questionArray);
+
         answer2.innerHTML =
             questionArray[Math.floor(Math.random() * questionArray.length)];
         questionArray = questionArray.filter((e) => e !== answer2.innerText);
-        // console.log(questionArray);
+
         answer3.innerHTML =
             questionArray[Math.floor(Math.random() * questionArray.length)];
         questionArray = questionArray.filter((e) => e !== answer3.innerText);
-        // console.log(questionArray);
+
         answer4.innerHTML =
             questionArray[Math.floor(Math.random() * questionArray.length)];
+
+        // Changes value of radio inputs to match AJAX answers
+        $('#a1').attr('value', answer1.innerText);
+        $('#a2').attr('value', answer2.innerText);
+        $('#a3').attr('value', answer3.innerText);
+        $('#a4').attr('value', answer4.innerText);
     } else if (res.results[index].type === 'boolean') {
         return;
+    }
+}
+
+function validateAnswer(res, index, selected) {
+    var correct = res.results[index].correct_answer;
+    console.log(correct, selected);
+    if (selected === correct) {
+        score++;
+        scoreElem.innerHTML = 'Your score: ' + score;
     }
 }
 
@@ -57,18 +71,18 @@ $.ajax({
     populateQuiz(res, index);
 
     $('#submit').click(() => {
+        const radio = document.querySelector('input[type="radio"]:checked');
+        var selected = radio.value;
+        console.log('selected answer: ' + selected);
+        validateAnswer(res, index, selected);
         index++;
-        populateQuiz(res, index);
-    });
 
-    $('input').click(() => {
-        const radio = $('input[type="radio"]:checked'.siblings('label').val());
-        var selected = radio.val();
-        console.log(selected);
-        return selected;
+        if (index > 9) {
+            location.replace('/end');
+            console.log('end of quiz');
+        } else {
+            populateQuiz(res, index);
+        }
+        radio.checked = false;
     });
-    console.log(selected);
-    if (selected === res.results[index].correct_answer) {
-        return score++;
-    }
 });
