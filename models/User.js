dc5e02fec33dd5495c76581c100ae5e2a6435bdd
Jeremiah Holden <jeremiah@cardconnectcp.com>
2021-01-08 21:13:08 -0800

    trying to fix high_score

--- models/User.js
+++ models/User.js
@@ -60,7 +60,7 @@ User.init(
         sequelize,
         timestamps: false,
         freezeTableName: true,
-        underscored: true,
+        // underscored: true,
         modelName: 'user',
     }
 );
